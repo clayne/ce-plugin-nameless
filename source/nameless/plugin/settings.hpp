54e5f34d5a5b5b1a0c5b512d0586e9d297c8aa8e
dove-zp <89095890+dove-zp@users.noreply.github.com>
2022-02-10 20:47:53 -0500

    default

---
+++ source/nameless/plugin/settings.hpp
@@ -0,0 +1,21 @@
+/**
+ * \brief   plugin - imports
+ * \author  dove-zp
+ * \contact https://github.com/dove-zp
+ * \version 2020/OCT/24
+ * \license BSD 3-Clause "New" or "Revised" License
+ */
+
+//--------------------------------------------------------------------------------------------------
+
+#pragma once
+#include <string>
+
+//--------------------------------------------------------------------------------------------------
+
+struct _settings
+{
+	bool random_generation;
+	int refresh_rate_ms;
+	std::string window_name;
+};
